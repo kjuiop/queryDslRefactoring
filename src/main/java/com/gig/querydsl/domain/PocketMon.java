b6a95b635525bc042a2675504790f5fe0b7f7c57
kjuiop <copying_y@naver.com>
2020-10-25 12:46:48 +0900

    Initial commit

---
+++ src/main/java/com/gig/querydsl/domain/PocketMon.java
@@ -0,0 +1,19 @@
+package com.gig.querydsl.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class PocketMon {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
---
+++ src/test/java/com/gig/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gig.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+public class QuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
