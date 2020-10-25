e17d0e54d8bf65a2ebc62aaeeaaf4b17f12224cc
kjuiop <copying_y@naver.com>
2020-10-25 13:28:09 +0900

    querydsl 개발환경구성

--- src/test/java/com/gig/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/gig/querydsl/QuerydslApplicationTests.java
@@ -1,17 +1,37 @@
 package com.gig.querydsl;
 
+import com.gig.querydsl.domain.PocketMon;
+import com.gig.querydsl.domain.QPocketMon;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@Commit
 public class QuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        PocketMon pocketMon = new PocketMon();
+        em.persist(pocketMon);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QPocketMon qPocketMon = new QPocketMon("p");
+
+        PocketMon result = query.selectFrom(qPocketMon).fetchOne();
+        Assertions.assertThat(result).isEqualTo(pocketMon);
+        Assertions.assertThat(result.getId()).isEqualTo(pocketMon.getId());
     }
 }
